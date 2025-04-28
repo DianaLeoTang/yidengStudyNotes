f7676fd84dc5fa6aacdcf154b63fad15ce127305
19801191517 <tangwangyao0228@163.com>
2025-04-28 12:13:57 +0800

    feat: Notes on Learning Cocos

---
+++ cocos2D/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Diana Tang
+ */
+
+// 1下载cocos2d-x-4.x.x.zip
+// 2解压
+// 3安装cocos2d-x-4.x.x.zip
+// 4进入cocos2d-x-4.x.x目录
+// 5运行cocos2d-x-4.x.x/setup.py  (mac自带python，windows需要安装python)
+// 6安装JDK
+// 7安装SDK
+// 8安装NDK
+// 9初始化一个项目
+// 10进入项目目录
+// 11连接真机，开启调试模式
+// 12运行项目
